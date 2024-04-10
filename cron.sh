50371f9ca1abc32b7fd7929c2a893b02ef7dc8aa
Jonny McCullagh <jonny.mccullagh@redbranch.net>
2024-04-10 19:16:36 +0100

    update homepage link

--- cron.sh
+++ cron.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /var/www/reports.openstreetmap.ie
 echo "fetching the latest .... "
+mkdir -p /var/www/reports.openstreetmap.ie/data
 s3cmd get --force s3://osmie-tiles/reports/* /var/www/reports.openstreetmap.ie
+s3cmd get --force s3://osmie-tiles/reports/data/* /var/www/reports.openstreetmap.ie/data
